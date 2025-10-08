629e802dccb339c8a734e129088a10ba57cdf898
hiashyr <shlimakovvvova@gmail.com>
2025-10-08 07:04:54 +0300

    Add page's templates

---
+++ bookworm/bookworm/urls.py
@@ -0,0 +1,12 @@
+from django.contrib import admin
+from django.urls import path
+from main import views
+
+urlpatterns = [
+    path('admin/', admin.site.urls),
+    path('', views.home, name='home'),
+    path('about/', views.about, name='about'),
+    path('contact/', views.contact, name='contact'),
+    path('login/', views.login_view, name='login'),
+    path('register/', views.register, name='register'),
+]
